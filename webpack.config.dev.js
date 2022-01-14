10973bdb9ded6acd72674a3c7233b7cffc09c1b8
Victor Bolaños <viktorb.132@gmail.com>
2022-01-14 00:25:33 -0600

    Modo development, limpieza modo produccion

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,10 +3,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -15,6 +12,8 @@ module.exports = {
         filename: '[name].[contenthash].js',
         assetModuleFilename: 'assets/images/[hash][ext][query]'
     },
+    mode: 'development',
+    watch: true,
     resolve: {
         extensions: ['.js'],
         alias: {
@@ -82,16 +81,6 @@ module.exports = {
                 }
             ]
         }),
-        new Dotenv(),
-        new CleanWebpackPlugin()
-    ],
-    optimization: {
-        minimize: true,
-        minimizer: [
-            // Optimizacion para css
-            new CssMinimizerPlugin(),
-            // Optimizacion para js
-            new TerserPlugin()
-        ]
-    }
+        new Dotenv()
+    ]
 }
